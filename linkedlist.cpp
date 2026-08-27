1183c04f58b9cec5df51b4f5ed5262cf76c156c1
Tejas <imtejaskhedkar@gmail.com>
2026-08-27 10:30:39 +0530

    Enhance linked list operations with new options

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -11,16 +11,17 @@ class Node
 
 int main()
 {
-	int action, n;
+	int action, action2, n;
 	Node *head = NULL;
 	Node *temp;
 	
 	
 	do{
 		cout<<"\n";
-		cout<<"\n1. Add node";
+		cout<<"\n1. Add node (at the end)";
 		cout<<"\n2. Display the list";
-		cout<<"\n3. Exit \n";
+		cout<<"\n3. Add node (Anywhere)";
+		cout<<"\n4. Exit \n";
 		
 		cin>>action;
 		switch(action){
@@ -30,6 +31,7 @@ int main()
 				
 			
 				Node *NN = new Node();
+				cout<<"Enter an integer: ";
 				cin>>n;
 				NN->data = n;
 				NN->next = NULL;
@@ -69,14 +71,58 @@ int main()
 				}
 			break;
 			}
+			
+			case 3:
+				{
+					cout<<"\nWhere do you want to add the node?\n";
+					cout<<"1. Start, 2. Middle, 3. End\n";
+					cin>>action2;
+					
+					switch(action2)
+					{
+						case 1:
+							{
+								// add node at start
+								break;
+							}
+						case 2:
+							{
+								// search and add node after that place
+								break;
+							}
+						case 3:
+							{
+								Node *NN = new Node();
+								cout<<"Enter an integer: ";
+								cin>>n;
+								NN->data = n;
+								NN->next = NULL;
+				
+								if(head==NULL)
+								{
+									head = NN;
+								}
+								else{
+									Node *temp = head;
+									while(temp->next!=NULL)
+									{
+										temp = temp->next;
+									}
+									temp->next = NN;
+								}
+								break;
+							}
+					}
+					
+					break;
+				}
 			default:
 				{
 					cout<<"\ninvalid choice";
+					break;
 				}
 				
 		}
-	}while(action!=3);
-	
-	
+	}while(action!=4);
 	
 }
